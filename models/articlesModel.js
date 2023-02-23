fcb2377a6f57af1ec6cb0493f8b6c70dbd9be363
reemhd <reemhde@gmail.com>
2023-02-23 09:44:10 +0000

    completed task12 DELETE

--- models/articlesModel.js
+++ models/articlesModel.js
@@ -96,3 +96,16 @@ exports.updateArticlesVotesInDB = (article_id, updatedVote) => {
       } else return rows[0];
     });
 };
+
+exports.deleteCommentByIdInDB = (comment_id) => {
+  const queryString = `
+  DELETE FROM comments 
+  WHERE comment_id = $1 
+  RETURNING *
+  `;
+  return db.query(queryString, [comment_id]).then((result) => {
+    if (result.rowCount === 0) {
+      return Promise.reject({ status: 404, message: "Comment not found" });
+    } else return;
+  });
+};
